9803a1f68ea72801358d115a427bed7e0880a16e
Mike Timko <timkomip@gmail.com>
2013-04-02 19:58:42 -0400

    Basic user auth and posts model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concord::Application.config.secret_token = '3eb52f4b4826c1a22c8b540917e6e9ebb5b0ce613c8d0a506ef4b2fd93b5999bae6e9a05244fb3e90eb875046102e0cd9019473aed1e399a4fd09b427a115adf'
